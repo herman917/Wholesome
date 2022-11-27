0c739ccd16c4b6cb1512674819607b3af3b07183
ChooseImage <chooseimage0125@gmail.com>
2022-11-27 17:32:57 -0500

    init new layout

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,15 @@
     $(function() {
         // this will get the full URL at the address bar
         var url = window.location.href;
-
+        console.log(window.location);
         // passes on every "a" tag
         $(".topmenu a").each(function() {
             // checks if its the same on the address bar
             if (url == (this.href)) {
                 $(this).closest("li").addClass("active");
                 //for making parent of submenu active
-              // $(this).closest("li").parent().parent().addClass("active");
+               $(this).closest("li").parent().addClass("active");
+               console.log((this).closest("li").parent());
             }
         });
     });        
